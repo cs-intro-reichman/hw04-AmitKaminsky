6ac4358bdb5e61b0d10f7b383357c3f88e9535cd
AmitKaminsky <amit.kaminsky@post.runi.ac.il>
2024-01-13 12:46:54 +0200

    Add all code

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,94 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+    findMissingInt(new int[] {1});
+    secondMaxValue(new int[] {0, 0, 1, 2});
+    containsTheSameElements(new int[] {1, 5}, new int[] {5, 1, 5, 1, 5});
+    isSorted(new int[] {1, -2, 3});
     }
     
+    
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int missingInt = -1;
+        boolean isMissing = false;
+        for (int arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+            for (int i = 0; i < array.length; i++) { 
+                /* Check if the value is in the array, if yes then
+                 * continue to the next value in given array.
+                 */
+                if (i != array[arrayIndex]) {
+                    isMissing = true;
+                    missingInt = i;
+                } else if (i == array[arrayIndex]) {
+                    isMissing = false;
+                    missingInt = 1;
+                    continue;
+                }
+            }
+            if (isMissing == true) {
+                break;
+            }
+        }
+        return missingInt;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int maxInt = 0;
+        int secondMaxInt = 0;
+        // Find maximum value
+        for (int arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+            maxInt = array[arrayIndex] > maxInt ? array[arrayIndex] : maxInt;
+        }
+        // Find second maximum value compared to the max
+        for (int arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+            if ((array[arrayIndex] < maxInt) && array[arrayIndex] > secondMaxInt) {
+                secondMaxInt = array[arrayIndex];
+            }
+        }
+        return secondMaxInt;
     }
 
+        // {1, 2, 3, 2} {2, 1}
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int[] smallSet = array1.length < array2.length ? array1 : array2;
+        int[] bigSet = array1.length > array2.length ? array1 : array2;
+        boolean isSameSet = false;
+        int count = 0;
+        for (int i = 0; i < bigSet.length; i++) {
+            for (int j = 0; j < smallSet.length; j++) {
+                if (bigSet[i] != smallSet[j]) {
+                    count++;
+                } else if (bigSet[i] == smallSet[j]) {
+                    count = 0;
+                    isSameSet = true;
+                    break;
+                }
+                if (count == smallSet.length) {
+                    return isSameSet = false;
+                }
+            }
+        }
+        return isSameSet;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        /* Divide to two events
+         * 1. itr each number and check if the next one is bigger or the same
+         * 2. if 1 didnt work, check if the next one is smaller or the same
+         */
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+        boolean isNotBoth = false;
+        for (int i = 0; i < array.length - 1; i++){
+            if (array[i] == array[i + 1]) {
+                continue;
+            } else if ((array[i] < array[i + 1]) && isIncreasing) {
+                isDecreasing = false;
+            } else if ((array[i] > array[i + 1]) && isDecreasing) {
+                isIncreasing = false;
+            } else if ((isIncreasing && !isDecreasing) || (!isIncreasing && isDecreasing)) {
+                return isNotBoth;
+            }
+        }
+        return isDecreasing || isIncreasing;   
     }
-
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,113 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        capVowelsLowRest("Hello World");
+        camelCase("   Intro  to   coMPUter    sCIEncE ");
+        allIndexOf("Hello world",'l'); // output: {2, 3, 9}
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String vowels = "aeiou";
+        String withBigVowels = "";
+        boolean isVowel = false;
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = 0; j < vowels.length(); j++) {
+                if (string.charAt(i) == vowels.charAt(j)) {
+                    withBigVowels += (char) (string.charAt(i) - 32);
+                    isVowel = true;
+                }
+            }
+            if ((string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') && !isVowel) {
+                withBigVowels += (char) (string.charAt(i) + 32);
+            } else if (!isVowel) {
+                withBigVowels += string.charAt(i);
+            }
+            isVowel = false;
+        }
+        System.out.println(withBigVowels);
+        return withBigVowels;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String camelCaseStr = "";
+        int j = 0;
+        boolean nextWord = false;
+        do {
+            if (string.charAt(j) == ' ') {
+                j++;
+            }
+        } while (j < string.length() && string.charAt(j) == ' ');
+        // Change first appearing letter to lowercase or if it's lowercase then add it
+        if (string.charAt(j) >= 'A' && string.charAt(j) <= 'Z') {
+                camelCaseStr += (char) (string.charAt(j) + 32);
+        } else if (string.charAt(j) >= 'a' && string.charAt(j) <= 'z') {
+            camelCaseStr += string.charAt(j);
+        }
+        for (int i = j + 1; i < string.length(); i++) {
+            // if it's a new word (was a space before it) then check if capital or lowercase
+            if (nextWord && (string.charAt(i) >= 'a' && string.charAt(i) <= 'z')) {
+                camelCaseStr += (char) (string.charAt(i) - 32);
+                nextWord = false;
+            } else if (nextWord && (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z')){
+                camelCaseStr += string.charAt(i);
+                nextWord = false;
+            // if it's still the same word = change capital letters to lowercase
+            } else if (!nextWord && (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z')) {
+                camelCaseStr += (char) (string.charAt(i) + 32);
+            } else if (!nextWord && (string.charAt(i) >= 'a' && string.charAt(i) <= 'z')) {
+                camelCaseStr += string.charAt(i);
+            // if it's not a letter but a 'space' - than mark it a new word
+            } else if (string.charAt(i) == ' ') {
+                nextWord = true;
+            }
+        }
+        System.out.println(camelCaseStr);
+        return camelCaseStr;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        /* The func receives a (not empty) string and at least once appearing char in it
+         * first itr in the string and count the given char
+         * then itr again and check what is the index of it 
+         */
+        int countChar = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                countChar++;
+            }
+        }
+        // "Hello World"
+        int[] charAppearance = new int[countChar];
+        int subStrIndex = 0;
+        int i = 0;
+        int originalStrIndexCount = 0;
+        String subStr = string;
+        while (subStr.indexOf(chr) != -1) {
+            if (subStrIndex == subStr.indexOf(chr)) {
+                charAppearance[i] = originalStrIndexCount + subStr.indexOf(chr); // 0 + 2 = 2
+                subStr = subStr.substring(subStrIndex + 1); // "lo World" > "o World" > "d"
+                originalStrIndexCount += subStrIndex + 1; // 2 + 1 = 3
+                subStrIndex = 0;
+                i++;
+            } else {
+                subStrIndex++;
+            }
+        }
+        System.out.print("output: ");
+        printArray(charAppearance);
+        System.out.println("");
+        return charAppearance;
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.print("{");
+        for (int i = 0; i < arr.length; i++){
+            if (i != arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+                
+            } else {
+                System.out.print(arr[i] + "}");
+            }
+        }
     }
 }
